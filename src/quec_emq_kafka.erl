f62b99ca083ea28941e419684cae7fff35f368d0
jaren.han <jaren.han@quectel.com>
2018-10-22 16:26:51 +0800

    add pkgid

--- src/quec_emq_kafka.erl
+++ src/quec_emq_kafka.erl
@@ -149,6 +149,7 @@ ekaf_send(KafkaTopic,Type, ClientId, {Topic, Opts}, _Env) ->
   ekaf_send_async(KafkaTopic,Json);
 ekaf_send(KafkaTopic,Type, _, Message, _Env) ->
   Id = Message#mqtt_message.id,
+  PkgId=Message#mqtt_message.pkgid,
   From = Message#mqtt_message.from, %需要登录和不需要登录这里的返回值是不一样的
   Topic = Message#mqtt_message.topic,
   Payload = Message#mqtt_message.payload,
@@ -164,7 +165,7 @@ ekaf_send(KafkaTopic,Type, _, Message, _Env) ->
     {type, Type},
     {client_id, ClientId},
     {message, [
-      {messsaseid,i(Id)},
+      {pkgId,PkgId},
       {username, Username},
       {topic, Topic},
       {payload, Payload},
